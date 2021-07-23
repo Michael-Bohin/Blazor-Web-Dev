aa7abe1b53b3f85d2019b01be238ead10388da92
Michael Bohin <mbohin@seznam.cz>
2021-07-23 20:28:34 +0200

    Creating solutions and object architecture skeleton

---
+++ 01 - CLI Infinite generator/CLI View of Infinite Engine library/CLI View.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static System.Console;
+
+namespace CLI_View_of_Infinite_Engine_library {
+    class Program {
+        static void Main() {
+            WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 01 - CLI Infinite generator/Infinite Engine/Math Representation.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace InfiniteEngine {
+    public abstract class Expression { }
+}
