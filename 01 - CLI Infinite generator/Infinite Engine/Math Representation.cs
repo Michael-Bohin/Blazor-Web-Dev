42f8e8ed5ce0745dc5ac006697042edb56f689aa
Michael Bohin <mbohin@seznam.cz>
2021-07-25 14:28:05 +0200

    Changed Fraction's parent from Value to BinaryExpression where it belongs. Written its first implementation, method will likely be added over time to support Fraction arithemtic operations.
    Written implementation of class Variable. I am not happy with the DeepCopy method code. Although I will  rewrite it after going through options.

--- 01 - CLI Infinite generator/Infinite Engine/Math Representation.cs
+++ 01 - CLI Infinite generator/Infinite Engine/Math Representation.cs
@@ -10,7 +10,7 @@ namespace InfiniteEngine {
      *     Each comment should also clearly mention the part of expression that changed - 
      * isolate the change visually and discard not changed part of the expression. 
      *     For web and unity views, it will be important to develop interactive and visual 
-     * explanations. 
+     * explanations.
      */
     public abstract class Excercise { 
         public Expression Problem { get => steps[0]; }
@@ -53,7 +53,9 @@ namespace InfiniteEngine {
 
     public abstract class Value : Expression { }
 
-    public abstract class Constant : Value { }
+    public abstract class Constant : Value {
+        public ValueType number;
+    }
 
     public class Addition : BinaryExpression {
         protected override string SignRepresentation => "+";
@@ -71,12 +73,37 @@ namespace InfiniteEngine {
         protected override string SignRepresentation => "-";
     }
 
+    /*     Yes, Division and Fraction as part of math is the same thing and so if it only 
+     * depended on math only one class would be required. 
+     * 
+     *     However in the field of mathematics use of Fractions is so central, that specific 
+     * definitions set, invented by humans, has evolved that means humans use two names for division
+     * Divison and Fraction. Division in Czech textbooks has two different operators: '/' and ':'. 
+     * '/' means Fraction and ':' means Division. Both of them are the same arithmetic operation. 
+     * 
+     *     I will define both classes in math representation too. My point is to improve peoples'
+     * user experience learning math. This comes at the cost of writing repetitive code: arithemtic
+     * operation division will have two different classes that are describing it Fraction and Division.
+     * 
+     *     Question/Observation: Wouldn't it be easier for kids to understand the concept if the naming 
+     * duality would be removed? 
+     */
+
+    public class Fraction : BinaryExpression {
+        public Expression numerator;
+        public Expression denominator;
+        public new Expression rightOperand { get => numerator; } // c s tim naming rule violation => vskutku predtim to byl field ale na redirect na citatele a jemnovatele potrebuju property...
+        public new Expression leftOperand { get => denominator; }
+
+        protected override string SignRepresentation => "/";
+    }
+
     public class Minus : UnaryExpression {
         protected override string SignRepresentation => "-";
     }
 
     public class Integer : Constant {
-        public readonly int number;
+        public new readonly int number;
 
         public Integer(int i) {
             number = i;
@@ -88,7 +115,7 @@ namespace InfiniteEngine {
     }
 
     public class RealNumber : Constant {
-        public readonly double number;
+        public new readonly double number;
 
         public RealNumber(double d) {
             number = d;
@@ -99,8 +126,30 @@ namespace InfiniteEngine {
         public override RealNumber DeepCopy() => (RealNumber)this.MemberwiseClone();
     }
 
-    public class Fraction : Value { }
+    public class Variable : Value {
+        public readonly string variableName;
+        public readonly Constant constant;
 
-    public class Variable : Value { }
+        public Variable(Constant c, string s) {
+            variableName = s;
+            constant = c;
+        }
 
+        public override string ToString() => $"{constant}{variableName}";
+
+        // I dislike the current implementation of DeepCopy, I will think through options and rewrite it. 
+        public override Variable DeepCopy() {
+            // using Microsoft's docs recomended approach:
+            // https://docs.microsoft.com/en-us/dotnet/api/system.string.copy?view=net-5.0
+            string otherName = variableName[..];
+            if (constant is Integer i) {
+                Integer otherConstant = new Integer(i.number);
+                return new Variable(otherConstant, otherName);
+            } else if( constant is RealNumber r) {
+                RealNumber otherConstant = new(r.number);
+                return new Variable(otherConstant, otherName);
+            }
+            throw new Exception("DeepCopy method of Variable class encountered unknown Constant's child.");
+        }
+    }
 }
